1d8c733bdf76a1357d222bd29de4454da9d84d88
LemurdurerC <claudomirkoda@gmail.com>
2021-06-05 02:10:08 +0200

    reception d'image + conversion bytes en obj image

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import base64
+import string
+
+import numpy as np
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import shutil
+
+import cv2
+
 
 app = FastAPI()
 
-@app.get("/")
-async def hello_world():
-    return {"hello" : "world"}
+@app.post("/")
+async def create_upload_file(file: UploadFile = File(...)):
+    test = file.file.read()
+    test2 = np.frombuffer(test,np.uint8)
+    img = cv2.imdecode(test2,cv2.IMREAD_COLOR)
+    cv2.imshow("test", img)
+    cv2.waitKey()
+
+    return {"filename": file.filename}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
