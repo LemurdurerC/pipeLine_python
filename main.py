132ca60108670a3fbebae12cfef3d44ef7e9943e
LemurdurerC <claudomirkoda@gmail.com>
2021-06-06 02:34:50 +0200

    accepte la requête http fetch

--- main.py
+++ main.py
@@ -5,21 +5,43 @@ import numpy as np
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 import shutil
+from fastapi.middleware.cors import CORSMiddleware
 
 import cv2
 
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.post("/")
 async def create_upload_file(file: UploadFile = File(...)):
     test = file.file.read()
     test2 = np.frombuffer(test,np.uint8)
-    img = cv2.imdecode(test2,cv2.IMREAD_COLOR)
-    cv2.imshow("test", img)
-    cv2.waitKey()
+    try:
+        img = cv2.imdecode(test2,cv2.IMREAD_COLOR)
+        print("ok")
+        cv2.imshow("test", img)
+        cv2.waitKey()
+    except Exception as e:
+        print("Lol")
+    return "test"
 
-    return {"filename": file.filename}
+    # appeler le reste du pipeline
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
